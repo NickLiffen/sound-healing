2f5fc76d946f71606629643ff7c3ff59136d41da
Nick Liffen <UP653591@myport.ac.uk>
2014-10-21 22:16:58 +0100

    Contact Form Validation

--- contactpage.php
+++ contactpage.php
@@ -1,43 +1,45 @@
 <?php $title="Contact Page" ; include( "inc/header.php"); ?>
 
-                <h1 id="contactwelcome">Get In Touch</h1>
+        <artlcle>
+          <section>
+              <h1 id="contactwelcome">Get In Touch</h1>
 
                 <!-- This form send the data through multipart form data (Ajax Request),
                 and stores it in a database. It collects the inputted data from the user
                 through the ID's of each section. If there is an error it prints it out
                 in the error* part of that section E.G errorfirstname.-->
 
-                <form enctype="multipart/form-data" id="myForm" method="post"
+                <form enctype="multipart/form-data" id="contactForm" method="post"
                 name="myForm" onsubmit="return false">
 
                     <!-- First Name Field -->
-                    <p>First Name<br><input id="firstname" name=
+                    <p>First Name: <input id="firstname" name=
                     "firstname" type="text" autofocus> <span id=
                     "errorfirstname"></span> <span class=
                     "error"></span></p>
 
                     <!-- Last Name Field -->
-                    <p>Last Name <br><input id="lastname" name=
+                    <p>Last Name: <input id="lastname" name=
                     "lastname" type="text"> <span id=
                     "errorlastname"></span> <span class=
                     "error"></span></p>
 
                     <!-- Email Field -->
-                    <p>Email<br><input id="email" name=
+                    <p>Email: <input id="email" name=
                     "email"  type="email"> <span id=
                     "erroremail"></span> <span class=
                     "error"></span></p>
 
                     <!-- Telephone Field-->
-                    <p>Telephone<br><input id=
+                    <p>Telephone: <input id=
                     "telephone" name="telephone" >
                     <span id="errortelephone"></span> <span class=
                     "error"></span></p>
 
                     <!-- Comments Field-->
-                    <p>Comments</p>
-                    <textarea id="Comment" rows="5"></textarea>
-                    <span id="errortelephone"></span> 
+                    <p>Comments: </p>
+                    <textarea id="comment" rows="5"></textarea>
+                    <span id="errorcomment"></span>
                     <span class="error"></span>
 
                     <!-- Submit Button.-->
@@ -45,6 +47,8 @@
                     value="Submit Form">
 
                   </form>
+            </section>
+          </article>
 
     <!--This is the AJAX function that adds the contact information to the database.-->
     <script src="js/addContactInfo.js"></script>
--- js/addContactInfo.js
+++ js/addContactInfo.js
@@ -6,3 +6,78 @@
 4. Throw back a success or error message depending on if the data has been stored successfully or not.
 
 */
+
+
+
+//Validates the Form that allows the user to enter a product to the database.
+validateForm = function () {
+
+  //Inititiates all the varibles that we are going to use to validae the form.
+  var errors, a, b, c, d, e, errorFirstName, errorLastName, errorEmail, errorTelephone, errorMessage = "";
+
+  //Sets errors to 0
+  errors = 0;
+
+  //Checks the First Name value of the form is entered.
+    a = document.forms["contactForm"]["firstname"].value;
+    if (a == null || a == "") {
+        errorFirstName = _('errorfirstname');
+        errorFirstName.style.color ='red';
+        errorFirstName.innerHTML = "Please enter your First Name";
+        errors = errors + 1;
+    }
+
+  //Checks the Last Name value is entered.
+    b = document.forms["contactForm"]["lastname"].value;
+    if (b == null || b == "") {
+        errorLastName = _('errorlastname');
+        errorLastName.style.color = 'red';
+        errorLastName.innerHTML = "Please enter your Last Name";
+        errors = errors + 1;
+    }
+
+  //Checks to see if the Email has been entered.
+    c = document.forms["contactForm"]["email"].value;
+    if (c == null || c == "") {
+        errorEmail = _('erroremail');
+        errorEmail.style.color = 'red';
+        errorEmail.innerHTML = "Please enter an Email";
+        errors = errors + 1;
+    }
+
+    //Checks the Telephone vaue has been entered.
+      d = document.forms["contactForm"]["telephone"].value;
+      if (d == null || d == "" || !isNumber(d)) {
+          errorTelephone = _('errortelephone');
+          errorTelephone.style.color = 'red';
+          errorTelephone.innerHTML = "Please enter a valid phone number";
+          errors = errors + 1;
+      }
+
+      //Checks the Telephone vaue has been entered.
+        e = document.forms["contactForm"]["comment"].value;
+        if (e == null || e == "") {
+            errorTelephone = _('errorcomment');
+            errorTelephone.style.color = 'red';
+            errorTelephone.innerHTML = "Please enter a Message";
+            errors = errors + 1;
+        }
+
+  //If there are any errors are found it returns false but if not it proccedds.
+    if (errors > 0) {
+        return false;
+    } else {
+      console.log(errors)
+        //uploadedProduct();
+    }
+}
+//Waits and Checks to see when the submit button has been pressed.
+pageLoaded = function () {
+  var fetchButton;
+    fetchbutton = _("submit");
+    if (fetchbutton) {
+        fetchbutton.addEventListener("click", validateForm);
+    }
+}
+//Event Listner for when the page loads.
+window.addEventListener("load", pageLoaded);
