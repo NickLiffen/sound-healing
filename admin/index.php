4f963615fb9a4e6375cefead25a2c131e5409b1c
LWrightson <b2015186@shu.ac.uk>
2014-12-15 21:11:45 +0000

    changes to admin css

--- admin/index.php
+++ admin/index.php
@@ -1,51 +1,44 @@
 <?php $title="Admin Page" ; include( "inc/header.php"); ?>
 
-<h1 class='center'> Staticsitcs about your website </h1>
+<h1 id="contactwelcome">Manage Your Content </h1>
 
 <div class='paddingBottom'></div>
-<section>
-<h3>Product Summary</h3>
-
-
-<div class='paddingBottom'></div>
-
-
-<ul>
+<div id = "admintabs">
+<section id = "admincontrols">
+<ul id = "adminboxes">
 
   <!--This prints the total number of: Services in the databse-->
-  <li>The Total amount of Services:
+  <li class = "tabs">Total Services
     <div id='totalServices'></div>
     <div class='paddingBottom'></div>
   </li>
 
   <!--This prints the total number of: Classes in the database-->
-  <li>The Total amount of Classes:
+  <li class = "tabs">Total Classes
     <div id='totalClasses'></div>
     <div class='paddingBottom'></div>
   </li>
 
   <!--This prints the total number of: Emails that the admin hasn't responded too-->
-  <li>The Total amount of Email's to Respond too:
-    <div id='emailStillToAswer'></div>
+  <li class = "tabs"> Unread Emails
+    <div id='emailStillToAnswer'></div>
     <div class='paddingBottom'></div>
   </li>
 
   <!--This prints the total number of:Emails that the admin has responded too-->
-  <li>The Total amount of Email's you HAVE responded too:
+  <li class = "tabs"> Total Replies
     <div id='emailAnswered'></div>
     <div class='paddingBottom'></div>
   </li>
 
 </ul>
 </section>
+</div>
 
 
 <div class='paddingBottom'></div>
 <section>
 
-  <!--This prints the total number of: Overall classes. It prints them out in a list. -->
-<p> Here is a list of all the classes you have on at the moment - please navigate to the Modify - Delete Page to change anything :)</p>
-<div class='paddingBottom'></div>
 <div id='allClasses'></div>
 
 </section>
