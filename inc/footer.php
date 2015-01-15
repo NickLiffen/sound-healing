019d18d25863c5e211257c484785784f0c1d4294
LWrightson <b2015186@shu.ac.uk>
2015-01-15 23:05:07 +0000

    further changes to the home page

--- inc/footer.php
+++ inc/footer.php
@@ -5,12 +5,17 @@
 </div>
       </body>
     <footer class = "site-footer">
+        <div id = "footerContainer">
     	<div class ="footerBox">
-    		 <div class='paddingBottom'></div>
-    		 <p> &#169; Copyright Shakty Sounds</p>
+    		  <div id = "socialFooter">
     		<a href="http://www.twitter.com/"><img src="img/twitter12.svg" width="75" height="75" alt="Link to Twitter" id='twitterImage' align="middle"></a>
     		<a href="http://www.facebook.com/"><img src="img/facebook29.svg" width="75" height="75" alt="Link the Facebook" id='facebookImage' align="middle"></a>
-    		
+            </div>
+            <div id ="copyFooter">
+                <p> &#169; Copyright Shakty Sounds</p>
+            </div>
+
     </div>
+</div>
   </footer>
 </html>
